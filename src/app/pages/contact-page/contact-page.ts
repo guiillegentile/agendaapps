f77cee047540edc8f7a4e480734e07ec3fa5389a
guillegentile <guillugen@gmail.com>
2025-10-20 15:22:59 -0300

    k

--- src/app/pages/contact-page/contact-page.ts
+++ src/app/pages/contact-page/contact-page.ts
@@ -1,32 +1,16 @@
-import { Component, inject, input } from '@angular/core';
-import { Contact } from '../../interfaces/contact';
-import { ContactsService } from '../../services/contacts-service';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import Swal from 'sweetalert2';
+import { CommonModule } from '@angular/common';
+import { ContactListItem } from '../../components/contact-list-item/contact-list-item';
+import { ContactsService } from '../../services/contacts-service';
 
 @Component({
-  selector: 'app-contact-list-item',
-  imports: [RouterModule],
-  templateUrl: './contact-list-item.html',
-  styleUrl: './contact-list-item.scss'
+  selector: 'app-contact-page',
+  standalone: true,
+  imports: [RouterModule, CommonModule, ContactListItem],
+  templateUrl: './contact-page.html',
+  styleUrl: './contact-page.css'
 })
-export class ContactListItem {
-  contact = input.required<Contact>();
-  aleatorio = Math.random();
-  contactsService = inject(ContactsService);
-
-  openDeleteModal(){
-    Swal.fire({
-      title: "¿Desea borrar el contacto?",
-      showDenyButton: true,
-      showCancelButton: true,
-      showConfirmButton: false,
-      cancelButtonText: "Cancelar",
-      denyButtonText: `Eliminar definitivamente`
-    }).then((result) => {
-      if (result.isDenied) { //Reviso que haya clickeado en el botón rojo.
-        this.contactsService.deleteContact(this.contact().id);
-      }
-    });
-  }
+export class ContactPage {
+  constructor(public contactsService: ContactsService) {}
 }
